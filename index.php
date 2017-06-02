09df584f748074f4a73fe0905e0f250b3ca6e078
baggerest <gg721115>
2017-06-03 02:25:38 +0800

    ...

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
+<script language="JavaScript">
+    <!--
+    var currentMenu = 1;
+    function ShowSubMenu(id) {
+        if (document.getElementById("SubMenu"+id).style.display == "")
+        {
+            document.getElementById("SubMenu"+id).style.display = "none";
+            currentMenu = 0;
+        }
+        else
+        {
+            if (currentMenu != 0)
+            {
+                document.getElementById("SubMenu"+id).style.display = "none";
+            }
+            document.getElementById("SubMenu"+id).style.display = "";
+            currentMenu = id;
+        }
+    }
+    -->
+</script>
+
 <?php
-header("Content-Type:text/html; charset=big5");
-$main_foder = "D:/rathena/conf/";
+header("Content-Type:text/html; charset=utf-8");
+$main_folder = "C:/my/rathena/conf/";
 
 $read_conf_list = array(
     "battle_athena.conf",
@@ -29,32 +51,65 @@ $read_conf_list = array(
     "battle/skill.conf",
     "battle/status.conf",
 );
-try
+$get_ = get_conf_set_list($main_folder,$read_conf_list);
+$split = array();
+echo "<div style='margin-left: auto;margin-right: auto'>";
+echo "<table align='center' border='1' style='border-style: dashed;border-color:#FFAC55;padding:5px;'>";
+$i = 0;
+foreach ($get_ as $name => $item)
 {
-    foreach ($read_conf_list as $value)
+    $i++;
+    echo "<tr><th colspan='3' style='background-color: darkkhaki' onclick='ShowSubMenu($i)'>".$name."</th></tr>";
+    foreach ($item as $number => $value)
     {
-        $file = fopen($main_foder.$value,"r");
-        if ($file)
-        {
-            echo "[".$value."]<br>";
-            $i = 0;
-            while (!feof($file))
-            {
-                $contents = fgets($file);
-                $contents = trim($contents);
-                $i++;
-                if($contents!=="" && substr($contents,0,2)!=='//')
-                {
-                    if(!strpos($contents,"//")) {
-                        echo $i . ")" . $contents . "<br>";
-                    }
-                }
-            }
-            fclose($file);
-        }
+        $split = explode(": ",$value);
+        echo "<tr id='SubMenu{$i}' style='display: none'>";
+        echo "<td id='SubMenu{$i}' style='text-align: center;display: none'>";
+        echo $number;
+        echo "</td>";
+        echo "<td id='SubMenu{$i}' align='right' style='display: none'>".$split[0]."</td>";
+        echo "<td id='SubMenu{$i}' style='display: none'>";
+        echo "<input  name='".$split[0]."' type='text' value='".$split[1]."' size= ".(strlen($split[1])+5).">";
+        echo "</td>";
+        echo "</tr>";
     }
 }
-catch (Exception $e)
+echo "</table></div>";
+
+
+function get_conf_set_list($main_folder,$read_conf_list)
 {
-    echo $e;
+    try
+    {
+        $conf_list = array();
+        foreach ($read_conf_list as $value)
+        {
+            $file = fopen($main_folder.$value,"r");
+            if ($file)
+            {
+                while (!feof($file))
+                {
+                    $contents = fgets($file);
+                    $contents = trim($contents);
+                    if($contents!=="" && substr($contents,0,2)!=='//')
+                    {
+                        if(!strpos($contents,"//"))
+                        {
+                            if(!strpos($contents,": "))
+                            {
+                                $contents .= " ";
+                            }
+                            $conf_list[$value][] = $contents;
+                        }
+                    }
+                }
+                fclose($file);
+            }
+        }
+        return $conf_list;
+    }
+    catch (Exception $e)
+    {
+        echo $e;
+    }
 }
