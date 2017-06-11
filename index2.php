a6da5c9c2a7c68f0fab0060677ff5311243a8d8a
baggerest <gg721115>
2017-06-12 00:34:46 +0800

    ...

--- index2.php
+++ index2.php
@@ -1,6 +1,6 @@
 <?php
 $main_folder = "D:/rathena/conf/";
-//$main_folder = "C:/my/rathena/conf/";
+$main_folder = "C:/my/rathena/conf/";
 
 $read_conf_list = array(
     0 => "battle_athena.conf",
@@ -71,28 +71,21 @@ $save_txt_ = array(
 
 function get_conf_set_list($main_folder,$read_conf_list)
 {
-    try
-    {
+    try {
         $conf_list = array();
-        foreach ($read_conf_list as $value)
-        {
-            $file = fopen($main_folder.$value,"r");
-            if ($file)
-            {
-                while (!feof($file))
-                {
+        foreach ($read_conf_list as $value) {
+            $file = fopen($main_folder . $value, "r");
+            if ($file) {
+                while (!feof($file)) {
                     $contents = fgets($file);
                     $contents = trim($contents);
-                    if($contents!=="" && substr($contents,0,2)!=='//')
-                    {
-                        if(!strpos($contents,"//"))
-                        {
-                            $sp[0] = substr($contents,0,strpos($contents,":"));
-                            $sp[1] = trim(substr($contents,strpos($contents,":")+1));
-                            if($sp[0]=='import')
-                            {
+                    if ($contents !== "" && substr($contents, 0, 2) !== '//') {
+                        if (!strpos($contents, "//")) {
+                            $sp[0] = substr($contents, 0, strpos($contents, ":"));
+                            $sp[1] = trim(substr($contents, strpos($contents, ":") + 1));
+                            if ($sp[0] == 'import') {
                                 $conf_list[$value][$sp[0]][] = $sp[1];
-                            }else{
+                            } else {
                                 $conf_list[$value][$sp[0]] = $sp[1];
                             }
                         }
@@ -102,79 +95,98 @@ function get_conf_set_list($main_folder,$read_conf_list)
             }
         }
         return $conf_list;
-    }
-    catch (Exception $e)
-    {
+    } catch (Exception $e) {
         echo $e;
     }
+}
+
+function go_import($setlist,$conf_file_list,$filename,$td_height_px,$save_txt_file_list,$save_txt_)
+{
+    $count = count($setlist);
+    $count += isset($conf_file_list[$filename]['import']) ? count($conf_file_list[$filename]['import']) - 1 : 0;
+    echo "<td rowspan='$count'><textarea id='$filename^import' name='$filename^import' spellcheck='false' style='height: " . ($count * $td_height_px) . "px' onchange='sameway(this.id);'>";
+    if (isset($save_txt_file_list[$save_txt_[$filename]]['import'])) {
+        $temp = $save_txt_file_list[$save_txt_[$filename]]['import'];
+        foreach ($temp as $importvalue) {
+            echo $importvalue . "\n";
+        }
+    }
+    echo "</textarea></td>";
 }
 
 $conf_file_list = get_conf_set_list($main_folder,$read_conf_list);
 $save_txt_file_list = get_conf_set_list($main_folder,$save_txt);
 
-echo "<table border='1'>";
-foreach ($conf_file_list as $filename => $setlist)
-{
+$td_height_px = 27;
+echo "<form action='' method='post'>";
+echo "<table id='set_table' border='1' align='center' style='border-style: dashed;border-color:#FFAC55;padding:5px;text-align: center'>";
+foreach ($conf_file_list as $filename => $setlist) {
     $check = true;
-    echo "<tr><th colspan='2'>$filename</th><th colspan='2'>$save_txt_[$filename]</th></tr>";
-    foreach ($setlist as $setname => $value)
-    {
-        if($setname!='import')
-        {
+    echo "<tr><th colspan='2' onclick='show_hide(\"$filename\");'>$filename</th><th colspan='2' onclick='show_hide(\"$filename\");'>$save_txt_[$filename]</th></tr>";
+    echo "<tbody id='$filename' style='display: none'>";
+    foreach ($setlist as $setname => $value) {
+        if ($setname != 'import') {
             echo "<tr>";
             echo "<td>$setname</td><td>$value</td>";
-            if(isset($save_txt_file_list[$save_txt_[$filename]][$setname]))
-            {
-                echo "<td><input type='text' value='{$save_txt_file_list[$save_txt_[$filename]][$setname]}' style='border: none'></td>";
-            }else{
-                echo "<td><input type='text' style='border: none'></td>";
-            }
-            if($check)
-            {
+            $inputid = $filename . "^" . $setname;
+            $inputvalue = isset($save_txt_file_list[$save_txt_[$filename]][$setname]) ? $save_txt_file_list[$save_txt_[$filename]][$setname] : "";
+            echo "<td><input type='text' id='$inputid' name='$inputid' value='$inputvalue' onkeyup='chedklengh(this.id);'></td>";
+            if ($check) {
                 $check = false;
-                $count = count($setlist);
-                if(isset($conf_file_list[$filename]['import']))
-                {
-                    $count += count($conf_file_list[$filename]['import'])-1;
-                }
-                echo "<td rowspan='$count'><textarea spellcheck='false' style='border: none;height: ".($count*27)."px'>";
-                if(isset($save_txt_file_list[$save_txt_[$filename]]['import']))
-                {
-                    $temp = $save_txt_file_list[$save_txt_[$filename]]['import'];
-                    foreach ($temp as $importvalue)
-                    {
-                        echo $importvalue."\n";
-                    }
-                }
-                echo "</textarea></td>";
+                go_import($setlist,$conf_file_list,$filename,$td_height_px,$save_txt_file_list,$save_txt_);
             }
             echo "</tr>";
-        }else{
-            foreach ($value as $listindex => $item)
-            {
-                echo "<tr><td>$setname</td><td colspan='1'>$item</td><td></td>";
-                if($check)
-                {
+        } else {
+            foreach ($value as $listindex => $item) {
+                echo "<tr><td>$setname</td><td>$item</td><td><input type='text' readonly></td>";
+                if ($check) {
                     $check = false;
-                    $count = count($setlist);
-                    if(isset($conf_file_list[$filename]['import']))
-                    {
-                        $count += count($conf_file_list[$filename]['import'])-1;
-                    }
-                    echo "<td rowspan='$count'><textarea spellcheck='false' style='border: none;height: ".($count*27)."px'>";
-                    if(isset($save_txt_file_list[$save_txt_[$filename]]['import']))
-                    {
-                        $temp = $save_txt_file_list[$save_txt_[$filename]]['import'];
-                        foreach ($temp as $importvalue)
-                        {
-                            echo $importvalue."\n";
-                        }
-                    }
-                    echo "</textarea></td>";
+                    go_import($setlist,$conf_file_list,$filename,$td_height_px,$save_txt_file_list,$save_txt_);
                 }
                 echo "</tr>";
             }
         }
     }
+    echo "</tbody>";
 }
 echo "</table>";
+echo "</form>";
+?>
+
+<script type="text/javascript">
+    function show_hide(id) {
+        document.getElementById(id).style.display = document.getElementById(id).style.display == 'none' ? '' : 'none';
+    }
+
+    function chedklengh(id) {
+        obj = document.getElementById(id);
+        obj.size = (obj.value.length > 24 ? obj.value.length : 20);
+    }
+
+    function sameway(id) {
+        arr = new Array(<?php foreach ($save_txt_ as $name => $savefile) {
+            if($savefile==$save_txt[0]) {
+                echo "\"$name^import\",";
+            }
+        } ?>);
+        for(var importtextarea in arr) {
+            if(arr.indexOf(id)!=-1) {
+                document.getElementById(arr[importtextarea]).value = document.getElementById(id).value;
+            }
+        }
+    }
+</script>
+
+<style>
+    th {
+        cursor: pointer;
+        background-color: #BDB885;
+    }
+    input {
+        border: none;
+        text-align: center;
+    }
+    textarea {
+        border: none;
+    }
+</style>
